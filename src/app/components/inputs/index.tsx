72acc317a5775aedbd4cd6830953f2115ddf1cfb
Ivan Roschin <ivan.roschin86@gmail.com>
2026-02-05 18:29:51 +0200

    add article

--- src/app/components/inputs/index.tsx
+++
@@ -1,9 +0,0 @@
-export { default as Textarea } from '@/app/components/inputs/Textarea';
-
-export { default as Select } from '@/app/components/inputs/Select';
-
-export { default as Checkbox } from '@/app/components/inputs/Checkbox';
-
-export { default as Input } from '@/app/components/inputs/Input';
-
-export { default as AutoSlugField } from '@/app/components/inputs/AutoSlugField';
