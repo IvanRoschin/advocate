4c8482c5b85499fa6d65c8413e526255c439c512
Ivan Roschin <ivan.roschin86@gmail.com>
2026-04-17 14:55:44 +0300

    fix upload img

--- src/app/lib/index.ts
+++ src/app/lib/index.ts
@@ -1,5 +1,4 @@
 export { generateTempPassword } from '@/app/lib/auth/generateTempPassword';
-export { default as CloudinaryUploadWidget } from '@/app/lib/client/CloudinaryUploadWidget';
 export { getArticleImageUrl } from '@/app/lib/cloudinary/article-image';
 export { getServiceImageUrl } from '@/app/lib/cloudinary/service-image';
 export { sendEmail } from '@/app/lib/server/mail/emailService';
